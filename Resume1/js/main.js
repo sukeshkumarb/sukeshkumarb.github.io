4869efb06dc130aa7e307f4108959df8ded38da2
Sukesh Batchu <sukeshkumarb@gmail.com>
2018-07-22 22:38:55 +0200

    changes

--- Resume1/js/main.js
+++ Resume1/js/main.js
@@ -90,7 +90,7 @@
 
     //typed js
     $(".typed").typed({
-        strings: ["My Name is M.Reza", "I'm a Web Designer", "Love Simplicity"],
+        strings: ["My Name is B.Sukesh", "I'm a Web / UI Developer"],
         typeSpeed: 100,
         backDelay: 900,
         // loop
